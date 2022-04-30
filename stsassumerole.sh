f875a47571004f8121047dbab26210625bdc1cbb
chinna44 <61354272+chinna44@users.noreply.github.com>
2022-04-30 09:28:48 +0530

    Update stsassumerole.sh

--- stsassumerole.sh
+++ stsassumerole.sh
@@ -1,9 +1,5 @@
 #!/bin/bash +x
 
-unset AWS_ACCESS_KEY_ID
-unset AWS_SECRET_ACCESS_KEY
-unset AWS_SESSION_TOKEN
-
 CREDDOC=$(aws sts assume-role --role-arn "arn:aws:iam::444843055205:role/jenkins_role" --role-session-name jenkins-execution-role)
 export AWS_ACCESS_KEY_ID=$( echo "${CREDDOC}" | jq -r '.Credentials.AccessKeyId' )
 export AWS_SECRET_ACCESS_KEY=$( echo "${CREDDOC}" | jq -r '.Credentials.SecretAccessKey' )
